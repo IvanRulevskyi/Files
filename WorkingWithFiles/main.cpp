2dea2a64102515363fbddb88cdf80f73383069fe
IvanRulevskyi <rulevskiy4@gmail.com>
2024-05-26 21:54:43 +0300

    Initial Commit

---
+++ WorkingWithFiles/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WorkingWithFiles
+//
+//  Created by Иван Рулевский on 26.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
